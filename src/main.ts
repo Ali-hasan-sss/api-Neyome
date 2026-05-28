c44170bd3870c24b7c0ccfd91591f07a3d1e0dbb
Ali hasan <160386537+Ali-hasan-sss@users.noreply.github.com>
2026-05-28 15:35:55 +0300

    add logo

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     origin: adminOrigins,
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-locale', 'Accept-Language'],
   });
 
   app.useGlobalPipes(
